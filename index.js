9dbcd7ac26bcab467a181d256b93e404bd2bcd2c
ngzhenghui94 <ngzhenghui94@hotmail.com>
2022-05-16 21:02:09 +0800

    Updated to send reminder to Telegram Grp Chat & Admin

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 import TelegramBot from "node-telegram-bot-api";
 import moment from "moment-timezone";
 moment.tz.setDefault("Asia/Singapore");
-import schedule from "node-schedule";
+import schedule from "node-schedule-tz";
 import dotenv from "dotenv";
 dotenv.config();
 
 const bot = new TelegramBot(process.env.TOKEN, { polling: true });
-const sendTo = process.env.TELEGRAMID;
-const myTelegramId = process.env.MYTELEGRAMID;
+const adminId = process.env.ADMINID;
+const telegramGroupId = process.env.TELEGRAMGROUPID;
 
+/* A cron job that runs first day of the month. */
+schedule.scheduleJob("* * 1  * * *", "Asia/Singapore", async () => {
+	bot.sendMessage(telegramGroupId, `Reminder to do your ${month} self-check`);
+	bot.sendMessage(
+		adminId,
+		`I have sent reminder to do your ${month} CTL Quiz. Thank you.`
+	);
+});
 
-/* A cron job that runs every 2 seconds. */
-const job = schedule.scheduleJob("2 * *  * * *", async () => {
-	bot.sendMessage("-598126299", "Hello World!");
-	console.log("Hello World!");
+/* A cron job that runs on the 20th day of the month. */
+schedule.scheduleJob("* * 20  * * *", "Asia/Singapore", async () => {
+	const getMonth = moment().format("MMMM");
+	bot.sendMessage(
+		telegramGroupId,
+		`Reminder to do your ${month} CTL Quiz. Thank you.`
+	);
+	bot.sendMessage(
+		adminId,
+		`I have sent reminder to do your ${month} CTL Quiz. Thank you.`
+	);
+});
+
+schedule.scheduleJob("* * 25  * * *", "Asia/Singapore", async () => {
+	const getMonth = moment().format("MMMM");
+	bot.sendMessage(
+		telegramGroupId,
+		`Reminder to do your ${month} CTL Quiz. Thank you.`
+	);
+	bot.sendMessage(
+		adminId,
+		`I have sent reminder to do your ${month} CTL Quiz. Thank you.`
+	);
+});
+
+schedule.scheduleJob("*/2 * *  * * *", async () => {
+	const month = moment().format("MMMM");
+	console.log(month);
+	bot.sendMessage(
+		telegramGroupId,
+		`Reminder to do your ${month} CTL Quiz. Thank you.`
+	);
+	bot.sendMessage(
+		adminId,
+		`I have sent reminder to do your ${month} CTL Quiz. Thank you.`
+	);
 });
