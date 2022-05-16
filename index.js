3b62765e7fabc76fc64a6238892df613eaf69878
ngzhenghui94 <ngzhenghui94@hotmail.com>
2022-05-16 21:09:14 +0800

    Minor update to scheduler*

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 const adminId = process.env.ADMINID;
 const telegramGroupId = process.env.TELEGRAMGROUPID;
 
-/* A cron job that runs first day of the month. */
-schedule.scheduleJob("* * 1  * * *", "Asia/Singapore", async () => {
+/* A cron job that runs on the first day of the month at 8am. */ 
+schedule.scheduleJob("* 8 1  * * *", "Asia/Singapore", async () => {
 	bot.sendMessage(telegramGroupId, `Reminder to do your ${month} self-check`);
 	bot.sendMessage(
 		adminId,
@@ -18,8 +18,8 @@ schedule.scheduleJob("* * 1  * * *", "Asia/Singapore", async () => {
 	);
 });
 
-/* A cron job that runs on the 20th day of the month. */
-schedule.scheduleJob("* * 20  * * *", "Asia/Singapore", async () => {
+/* A cron job that runs on the 20th day of the month at 8am.  */
+schedule.scheduleJob("* 8 20  * * *", "Asia/Singapore", async () => {
 	const getMonth = moment().format("MMMM");
 	bot.sendMessage(
 		telegramGroupId,
@@ -31,7 +31,8 @@ schedule.scheduleJob("* * 20  * * *", "Asia/Singapore", async () => {
 	);
 });
 
-schedule.scheduleJob("* * 25  * * *", "Asia/Singapore", async () => {
+/* A cron job that runs on the 25th day of the month at 8am. */
+schedule.scheduleJob("* 8 25  * * *", "Asia/Singapore", async () => {
 	const getMonth = moment().format("MMMM");
 	bot.sendMessage(
 		telegramGroupId,
@@ -43,15 +44,15 @@ schedule.scheduleJob("* * 25  * * *", "Asia/Singapore", async () => {
 	);
 });
 
-schedule.scheduleJob("*/2 * *  * * *", async () => {
-	const month = moment().format("MMMM");
-	console.log(month);
-	bot.sendMessage(
-		telegramGroupId,
-		`Reminder to do your ${month} CTL Quiz. Thank you.`
-	);
-	bot.sendMessage(
-		adminId,
-		`I have sent reminder to do your ${month} CTL Quiz. Thank you.`
-	);
-});
+// schedule.scheduleJob("*/10 * *  * * *", async () => {
+// 	const month = moment().format("MMMM");
+// 	console.log(month);
+// 	bot.sendMessage(
+// 		telegramGroupId,
+// 		`{Reminder to do your ${month} CTL Quiz. Thank you.}`
+// 	);
+// 	bot.sendMessage(
+// 		adminId,
+// 		`I have sent reminder to do your ${month} CTL Quiz. Thank you.`
+// 	);
+// });
