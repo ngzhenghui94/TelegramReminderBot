fbd36e1964bebde5594f6c81885a13a3a3c9d0ed
ngzhenghui94 <ngzhenghui94@hotmail.com>
2022-05-18 19:20:42 +0800

    push to repo

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 const adminId = process.env.ADMINID;
 const telegramGroupId = process.env.TELEGRAMGROUPID;
 
+
 /* A cron job that runs on the first day of the month at 8am. */
-schedule.scheduleJob("* 8 1  * * *", "Asia/Singapore", async () => {
+schedule.scheduleJob("* * 8 1 * *", async () => {
 	bot.sendMessage(
 		telegramGroupId,
 		`Reminder to do your ${month} self-check and ${month} CTL Quiz. Thank you`
@@ -23,7 +24,7 @@ schedule.scheduleJob("* 8 1  * * *", "Asia/Singapore", async () => {
 });
 
 /* A cron job that runs on the 20th day of the month at 8am.  */
-schedule.scheduleJob("* 8 20  * * *", "Asia/Singapore", async () => {
+schedule.scheduleJob("* 45 7  20 * *", async () => {
 	const month = moment().format("MMMM");
 	bot.sendMessage(
 		telegramGroupId,
@@ -36,7 +37,7 @@ schedule.scheduleJob("* 8 20  * * *", "Asia/Singapore", async () => {
 });
 
 /* A cron job that runs on the 25th day of the month at 8am. */
-schedule.scheduleJob("* 8 25  * * *", "Asia/Singapore", async () => {
+schedule.scheduleJob("* * 8 25 * *",  async () => {
 	const month = moment().format("MMMM");
 	bot.sendMessage(
 		telegramGroupId,
@@ -47,16 +48,3 @@ schedule.scheduleJob("* 8 25  * * *", "Asia/Singapore", async () => {
 		`I have sent reminder to do your ${month} CTL Quiz. Thank you.`
 	);
 });
-
-// schedule.scheduleJob("*/10 * *  * * *", async () => {
-// 	const month = moment().format("MMMM");
-// 	console.log(month);
-// 	bot.sendMessage(
-// 		telegramGroupId,
-// 		`{Reminder to do your ${month} CTL Quiz. Thank you.}`
-// 	);
-// 	bot.sendMessage(
-// 		adminId,
-// 		`I have sent reminder to do your ${month} CTL Quiz. Thank you.`
-// 	);
-// });
